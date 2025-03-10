faee8d0959542f54498b650c6421d7dc4bf21345
KevDeto <contactohuayta@gmail.com>
2025-03-09 21:40:29 -0300

    context agregado

---
+++ agencia/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using agencia.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace agencia.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+    }
+}
