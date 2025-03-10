429ecee0bc3d5980ffa7abcf7620893bc605a523
KevDeto <contactohuayta@gmail.com>
2025-03-09 21:42:18 -0300

    automapping terminado

---
+++ agencia/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using agencia.Models.Dto;
+using agencia.Models.Entity;
+using AutoMapper;
+
+namespace agencia
+{
+    public class MappingProfile : Profile
+    {
+        //Profiles
+        public MappingProfile()
+        {
+            CreateMap<PersonDTO, Person>();
+            CreateMap<Person, PersonDTO>();
+        }
+    }
+}
