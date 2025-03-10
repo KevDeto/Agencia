fc581a8ded54884af4c58255b8830642928b8691
KevDeto <contactohuayta@gmail.com>
2025-03-09 21:40:47 -0300

    Service terminado

---
+++ agencia/Services/Interfaces/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using agencia.Models.Dto;
+
+namespace agencia.Services.Interfaces
+{
+    public interface IPersonService
+    {
+        Task<PersonDTO> CreateAsync(PersonDTO personDTO);
+        Task<PersonDTO> GetByIdAsync(long Id);
+        Task UpdateAsync(PersonDTO personDTO);
+        Task DeleteAsync(long Id);
+        Task<IEnumerable<PersonDTO>> GetAllAsync();
+    }
+}
