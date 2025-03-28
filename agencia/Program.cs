119baa9535979984eab28a47cba20c9fdeb08e25
KevDeto <contactohuayta@gmail.com>
2025-03-28 09:19:53 -0300

    se quito cualquier referencia a Person

--- agencia/Program.cs
+++ agencia/Program.cs
@@ -9,48 +9,69 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Create a variable for the connection string
-var connectionString = builder.Configuration.GetConnectionString("Connection");
-// Register a service for connection
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddControllers();
+ConfigureDatabase(builder);
+ConfigureAutoMapper(builder);
+ConfigureRepositories(builder);
+ConfigureSwagger(builder);
+ConfigureMvc(builder);
 
-//Set up mapper
-//var mapperConfig = new MapperConfiguration(m =>
-//{
-//    m.AddProfile(new MappingProfile());
-//});
-//IMapper mapper = mapperConfig.CreateMapper();
-//builder.Services.AddSingleton(mapper);
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-builder.Services.AddMvc();
+var app = builder.Build();
 
-//repositoryes
-builder.Services.AddScoped<IPersonRepository, PersonRepository>();
-builder.Services.AddScoped<IPersonService, PersonService>();
-
-//Set up swagger
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();//error
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Agencia-API"));
-}
-
-//Middleware configuration
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+ConfigureMiddleware(app);
 
 app.Run();
+
+void ConfigureDatabase(WebApplicationBuilder builder)
+{
+    // Add services to the container.
+    // Create a variable for the connection string
+    var connectionString = builder.Configuration.GetConnectionString("Connection");
+    // Register a service for connection
+    builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+}
+
+void ConfigureAutoMapper(WebApplicationBuilder builder)
+{
+    // Register the AutoMapper service
+    builder.Services.AddAutoMapper(typeof(MappingProfile));
+}
+
+void ConfigureRepositories(WebApplicationBuilder builder)
+{
+    // Register the repository services
+    builder.Services.AddScoped<IClientRepository, ClientRepository>();
+    builder.Services.AddScoped<IClientService, ClientService>();
+    builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+    builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+}
+
+void ConfigureSwagger(WebApplicationBuilder builder)
+{
+    // Register the Swagger services
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddOpenApi();
+}
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    // Register the MVC services
+    builder.Services.AddControllers();
+    builder.Services.AddMvc();
+}
+
+void ConfigureMiddleware(WebApplication app)
+{
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+    {
+        app.MapOpenApi();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Agencia-API"));
+    }
+    //Middleware configuration
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
