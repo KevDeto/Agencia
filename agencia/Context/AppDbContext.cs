5b3ab381e8ea31c0cd48adac5fceb21527cc7720
KevDeto <contactohuayta@gmail.com>
2025-03-28 09:17:21 -0300

    se elimino Persona del dbContext

--- agencia/Context/AppDbContext.cs
+++ agencia/Context/AppDbContext.cs
@@ -5,12 +5,12 @@ namespace agencia.Context
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Person> Persons { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Employee> Employees { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
         {
 
         }
-
     }
 }
