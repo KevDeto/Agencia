7306570754ab75465564bd4ff5d340b67f3c0cef
KevDeto <contactohuayta@gmail.com>
2025-03-28 09:12:38 -0300

    DTOs terminados para cliente y empleado

---
+++ agencia/Models/Dto/ClientDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace agencia.Models.Dto
+{
+    public class ClientDTO
+    {
+        public long Id { get; set; }
+        public String name { get; set; }
+        public String lastName { get; set; }
+        [Required]
+        public String dni { get; set; }
+        [Required]
+        [EmailAddress]
+        public String email { get; set; }
+        public String telephone { get; set; }
+        public String adress { get; set; }
+        public String nacionality { get; set; }
+        public DateOnly birthDate { get; set; }
+    }
+}
--- agencia/Models/Dto/EmployeeDTO.cs
+++ agencia/Models/Dto/EmployeeDTO.cs
@@ -2,14 +2,21 @@
 
 namespace agencia.Models.Dto
 {
-    public class PersonDTO
+    public class EmployeeDTO
     {
         public long Id { get; set; }
         public String name { get; set; }
         public String lastName { get; set; }
+        [Required]
         public String dni { get; set; }
+        [Required]
+        [EmailAddress]
         public String email { get; set; }
         public String telephone { get; set; }
         public String adress { get; set; }
+        public String nacionality { get; set; }
+        public DateOnly birthDate { get; set; }
+        public String position { get; set; }
+        public decimal salary { get; set; }
     }
 }
