2f5f4a9972a6a6e98690fe13acad80b6a8da6872
KevDeto <contactohuayta@gmail.com>
2025-04-03 05:50:14 -0300

    se establecio las relaciones entre entidades y se agregaron los profile de Package, Service y Sale

--- agencia/Context/AppDbContext.cs
+++ agencia/Context/AppDbContext.cs
@@ -7,10 +7,41 @@ namespace agencia.Context
     {
         public DbSet<Client> Clients { get; set; }
         public DbSet<Employee> Employees { get; set; }
-
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Package> Packages { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //Configuration of the relationship between the entities
+
+            //Sale-Service (many-to-one)
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.service)
+                .WithMany(s => s.sales)
+                .HasForeignKey(s => s.serviceId)
+                .OnDelete(DeleteBehavior.Restrict);
+            //Sale-Package (many-to-one)
+            modelBuilder.Entity<Sale>()
+                .HasOne(s => s.package)
+                .WithMany(s => s.sales)
+                .HasForeignKey(s => s.packageId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Package-Service (many-to-many)
+            modelBuilder.Entity<Package>()
+                .HasMany(p => p.services)
+                .WithMany(s => s.packages)
+                .UsingEntity<Dictionary<string, object>>(
+                "Package_Service",
+                 c => c.HasOne<Service>().WithMany().HasForeignKey("ServiceId"),
+                 c => c.HasOne<Package>().WithMany().HasForeignKey("PackageId")
+                );
         }
     }
 }
