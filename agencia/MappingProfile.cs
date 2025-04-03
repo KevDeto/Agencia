b126f9f6c01a6d70e5a29dc081c8baa4a8726a2f
KevDeto <contactohuayta@gmail.com>
2025-04-03 05:44:42 -0300

    creado el mapeo de relaciones entre entidades

--- agencia/MappingProfile.cs
+++ agencia/MappingProfile.cs
@@ -15,6 +15,15 @@ namespace agencia
             CreateMap<EmployeeDTO, Employee>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore());
             CreateMap<Employee, EmployeeDTO>();
+            CreateMap<PackageDTO, Package>()
+                .ForMember(dest => dest.services, opt => opt.Ignore()) // Manejamos esto manualmente
+                .ForMember(dest => dest.Id, opt => opt.Ignore()); // Para updates
+            CreateMap<Package, PackageDTO>()
+                .ForMember(dest => dest.serviceIDs,
+                           opt => opt.MapFrom(src => src.services.Select(s => s.Id)));
+            CreateMap<ServiceDTO, Service>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            CreateMap<Service, ServiceDTO>();
         }
     }
 }
