d8806ed55bbcba198313a8b39c1c0066827c88cb
KevDeto <contactohuayta@gmail.com>
2025-04-08 17:00:04 -0300

    modificado para valores nullable

--- agencia/Models/Entity/Sale.cs
+++ agencia/Models/Entity/Sale.cs
@@ -6,10 +6,10 @@
         public DateTime date { get; set; }
         public double price { get; set; }
         //relationship with the Service entity
-        public long serviceId { get; set; }
-        public Service service { get; set; }
+        public long? serviceId { get; set; }
+        public Service? service { get; set; }
         //relationship with the Package entity
-        public long packageId { get; set; }
-        public Package package { get; set; }
+        public long? packageId { get; set; }
+        public Package? package { get; set; }
     }
 }
